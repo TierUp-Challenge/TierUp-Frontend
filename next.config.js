0eb72bc0f805efe036f8cd86491234a87fcc27da
imsmallgirl <d__wls_@naver.com>
2023-11-13 23:31:33 +0900

    [개발환경] next.config.js 설정 (#1)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// 서버 배포인지 로컬인지 확인
 
-module.exports = nextConfig
+const nextConfig = {
+  // React의 strict mode를 활성화하지 않음
+  reactStrictMode: false,
+
+  // Etag 비활성화
+  generateEtags: false,
+
+  // poweredByHeader 비활성화
+  poweredByHeader: false,
+
+  // 빌드 중에 ESLint를 무시함
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
